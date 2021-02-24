ec7da64a91006b5aded346daba4fab5f5d9c8d47
Jari Haapasaari <jthaapas@student.jyu.fi>
2021-02-24 17:16:58 +0200

    git init

---
+++ src/LeetCode.java
@@ -0,0 +1,29 @@
+public class LeetCode {
+
+    /*
+            Given a roman numeral, convert it to an integer.
+
+            I can be placed before V (5) and (X) (10) to make 4 and 9.
+            X can be placed before L (50) and (100) to make 40 and 90.
+            C can be placed before D (500) and M (1000) to make 400 and 900.
+
+     */
+
+    public static void main(String[] args) {
+        String s = "III";
+        String s2 = "IV";
+        
+        romanToInt(s2);
+    }
+
+    private static int romanToInt(String s2) {
+
+        int inputAsInteger =
+
+        if (1 < s2 && s2 < 10) {
+
+        }
+        return 0;
+
+    }
+}
