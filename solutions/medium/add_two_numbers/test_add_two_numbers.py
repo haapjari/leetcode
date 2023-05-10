ade43f0c0361335cc75b76906d87a72a41c3ec2d
Jari Haapasaari <haapjari@gmail.com>
2023-05-10 20:57:36 +0300

    WIP: Add Two Numbers

--- solutions/medium/add_two_numbers/test_add_two_numbers.py
+++ solutions/medium/add_two_numbers/test_add_two_numbers.py
@@ -19,12 +19,35 @@ class Solution:
 
         l3: ListNode
 
-        while l1:
-            print(f"l1 value {l1.val}")
-            print(f"l2 value {l2.val}")
+        # Create a temp node.
+        temp_node = ListNode()
+
+        # Keep track of the current node.
+        curr_node = temp_node
+
+        # TODO: Handle the case, if the sum is bigger than 9.
+        while l1 and l2:
+            sum = l1.val + l2.val
+
+            # if 0 <= sum <= 9:
+            #     print("sum is inbetween 0-9")
+            # else:
+            #     print("sum is greater than 9")
+
+            # print(f"sum {sum}")
+            new = ListNode(sum)
+            curr_node.next = new
+            curr_node = new
 
             l1 = l1.next
             l2 = l2.next
+
+        temp_node = temp_node.next
+
+        # This is the current Linked List
+        while temp_node:
+            print(temp_node.val)
+            temp_node = temp_node.next
 
 
 class TestSolution(unittest.TestCase):
