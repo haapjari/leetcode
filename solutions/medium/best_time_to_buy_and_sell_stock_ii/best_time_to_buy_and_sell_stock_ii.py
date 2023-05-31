376a870a2764372ee6a5758558685edbca4826e8
Jari Haapasaari <jari.haapasaari@nokia.com>
2023-05-31 18:06:14 +0300

    ROCKING IT

--- solutions/medium/best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.py
+++ solutions/medium/best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.py
@@ -1,38 +1,26 @@
 import unittest
 
 def maxProfit(prices: list) -> int:
-    """
-    :type prices: List[int]
-    :rtype: int
-    """
-
-    #Input: prices = [7, 1, 5, 3, 6, 4]
-    #Output: 7
-
-    # read a price
-    #
-    # if that price is smaller than yesterday == hold
-    # if that price is
-
-    priority = []
-
     profit = 0
-    owned = False
 
-    i = 0
+    # Start at the second day, because we need to compare
+    # to the first days stock price, to make a judgement.
+
+    i = 1
     while i < len(prices):
-        if i - 1
-        print(prices[i])
+        if prices[i - 1] < prices[i]:
+            profit = profit + prices[i] - prices[i - 1]
+
         i += 1
 
-    return 0
+    return profit
 
 class MaxProfitTestCase(unittest.TestCase):
     def test_1(self):
         prices = [7, 1, 5, 3, 6, 4]
         expected = 7
         result = maxProfit(prices)
-        #self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
     def test_2(self):
         prices = [1, 2, 3, 4, 5]
